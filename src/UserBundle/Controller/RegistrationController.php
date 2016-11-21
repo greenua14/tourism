aff854e7edc2ffe8dd0eebc2155737ca22ce11cf
greenua14 <yaroslav.artistsoft@gmail.com>
2016-11-21 21:52:17 +0200

    Added Registration Controller

---
+++ src/UserBundle/Controller/RegistrationController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace UserBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use UserBundle\Entity\User;
+use UserBundle\Form\UserType;
+
+class RegistrationController extends Controller
+{
+
+    public function registerAction(Request $request)
+    {
+
+        $oUser = new User();
+        $form = $this->createForm(UserType::class, $oUser);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $sPassword = $this->get('security.password_encoder')->encodePassword($oUser, $oUser->getPlainPassword());
+            $oUser->setPassword($sPassword);
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($oUser);
+            $em->flush();
+
+            return $this->redirectToRoute('user_registration');
+        }
+
+        return $this->render(
+            'UserBundle:Registration:register.html.twig',
+            ['form' => $form->createView()]
+        );
+
+    }
+
+}
