f7a6cdb034a364c2e445f74bfa4c854c4a5c4caa
greenua14 <yaroslav.artistsoft@gmail.com>
2016-11-26 20:13:11 +0200

    Added User entity

---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Class User
+ * @ORM\Entity
+ * @ORM\Table("fos_user")
+ */
+class User extends BaseUser
+{
+
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    public function __construct()
+    {
+
+        parent::__construct();
+
+    }
+
+}
